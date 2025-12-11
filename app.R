c056149d65ff57332aeb34435b9feb881792f96f
Julio Toboso <JulioToboso@gmail.com>
2025-12-11 20:55:52 +0100

    Update app.R

--- app.R
+++ app.R
@@ -1,1 +1,12 @@
+library(shiny)
+
+ui <- fluidPage(
+    # Aquí definiremos la interfaz completa
+)
+
+server <- function(input, output, session) {
+    # Aquí irá la lógica reactiva
+}
+
+shinyApp(ui, server)
 
