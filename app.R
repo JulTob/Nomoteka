2c5e31d559ab7c992138e3574d431dc00d154d28
Julio Toboso <JulioToboso@gmail.com>
2025-12-11 21:03:49 +0100

    Update app.R

--- app.R
+++ app.R
@@ -1,8 +1,32 @@
 library(shiny)
 
 ui <- fluidPage(
-    # Aquí definiremos la interfaz completa
-)
+    titlePanel("Name Vibe Checker:")
+    
+    textInput(
+                inputId = "input_name",
+                label   = "Type a name:",
+                placeholder = "e.g. Alejandro, Marco, Hans..."
+                )
+    
+    sidebarPanel(
+                textInput("nombre", "Name:", ""),
+                checkboxGroupInput(
+                                    "paises",
+                                    "Countries to compare:",
+                                    choices = c("Spain", "Germany", "Italy"),
+                                    selected = c("Spain", "Germany", "Italy")
+                                    )
+                )
+
+    mainPanel(
+                h3("Your name vibes score:"),
+                textOutput("summary"),
+                tableOutput("ranking_table"),
+                plotOutput("entropy_plot", height = "350px")
+                )
+
+    )
 
 server <- function(input, output, session) {
     # Aquí irá la lógica reactiva
